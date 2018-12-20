83795630c7f0ba484b4a9cb552f71dc884870762
inass <inesidrissi8@gmail.com>
2018-12-20 19:07:55 +0100

    add preferred stores

--- mall/urls.py
+++ mall/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
     # /music
     path('', views.index, name="index"),
     path('<int:shop_id>/', views.detail, name="detail"),
+    path('<shop_id>/favorite/', views.favorite, name='favorite'),
+    path('<shop_id>/nofavorite/', views.nofavorite, name='nofavorite'),
+    path('preferred/', views.preferred, name='preferred'),
 
 ]
--- mall/views.py
+++ mall/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render ,get_object_or_404
 from .models import Shop,Product
 
+
 def index(request):
+
     all_shops = Shop.objects.all()
     return render(request, 'mall/index.html', {'all_shops': all_shops})
 
@@ -10,4 +12,29 @@ def detail(request, shop_id):
 
     shop = get_object_or_404(Shop, pk=shop_id)
     return render(request, 'mall/detail.html', {'shop': shop})
+
+
+def favorite(request, shop_id):
+
+    all_shops = Shop.objects.all()
+    shop = get_object_or_404(Shop, pk=shop_id)
+    shop.is_favorite = True
+    shop.save()
+    return render(request, 'mall/index.html', {'all_shops': all_shops})
+
+
+def nofavorite(request, shop_id):
+
+    all_shops = Shop.objects.all()
+    shop = get_object_or_404(Shop, pk=shop_id)
+    shop.is_favorite = False
+    shop.save()
+    return render(request, 'mall/index.html', {'all_shops': all_shops})
+
+
+def preferred(request):
+
+    all_shops = Shop.objects.all()
+    return render(request, 'mall/preferred.html',{'all_shops': all_shops})
+
 
